0bb143f92231916fff30fb43f21b4b34ed220572
Ha1000tonG <ha1000tongs@gmail.com>
2025-10-01 16:54:59 -0300

    fix: Remove config específica do SQLite para o deploy

--- backend/database.py
+++ backend/database.py
@@ -7,9 +7,7 @@ from .config import settings  # <-- Importe 'settings'
 # Use a URL do banco de dados a partir das configurações
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
