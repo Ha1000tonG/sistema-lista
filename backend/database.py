c7bf689f353f63c2927b35eab65894357b937c0b
Ha1000tonG <ha1000tongs@gmail.com>
2025-10-08 00:24:55 -0300

    acrescentamos pool_pre_ping=True, pool_recycle=300

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,8 @@ from .config import settings  # <-- Importe 'settings'
 # Use a URL do banco de dados a partir das configurações
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       pool_pre_ping=True, pool_recycle=300)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
